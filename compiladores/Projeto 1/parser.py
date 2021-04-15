0f8751a85c3a01f8e8b73100de629ff1c2f61419
Rodrigo Pasquale <digoppasquale@msn.com>
2021-04-15 18:51:54 -0300

    Divisão de responsabilidades do módulo parser.py.

--- compiladores/Projeto 1/parser.py
+++ compiladores/Projeto 1/parser.py
@@ -1,297 +1,6 @@
-from rply.token import BaseBox
 from rply import ParserGenerator
-import lexer
-
-
-class BinaryOp(BaseBox):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
-
-
-class Integer_const(BaseBox):
-    def __init__(self, value):
-        self.value = value
-
-    def eval(self):
-        return self.value
-
-
-class Program(BaseBox):
-    def __init__(self, identifier, block):
-        self.identifier = identifier
-        self.block = block
-
-    def print(self):
-        self.identifier.print()
-        self.block.print()
-
-
-class Block(BaseBox):
-    def __init__(self, var_decl_part, subroutine_decl_part, statement_part):
-        self.var_decl_part = var_decl_part
-        self.subroutine_decl_part = subroutine_decl_part
-        self.statement_part = statement_part
-
-    def print(self):
-        self.var_decl_part.print()
-        self.subroutine_decl_part.print()
-        self.statement_part.print()
-
-
-class Variable_decl_part(BaseBox):
-    def __init__(self, variable_declaration, variable_declaration_list):
-        self.variable_declaration = variable_declaration
-        self.variable_declaration_list = variable_declaration_list
-
-    def print(self):
-        self.variable_declaration.print()
-        for var in self.variable_declaration_list:
-            var.print()
-
-
-class Variable_declaration(BaseBox):
-    def __init__(self, identifier, identifier_list, dtype):
-        self.identifier = identifier
-        self.identifier_list = identifier_list
-        self.dtype = dtype
-
-    def print(self):
-        self.identifier.print()
-        self.dtype.print()
-        for id_list in self.identifier_list:
-            id_list.print()
-
-
-class Index_range(BaseBox):
-    def __init__(self, beginning, ending):
-        self.beginning = beginning
-        self.ending = ending
-
-    def print(self):
-        self.beginning.print()
-        self.ending.print()
-
-
-class Procedure_decl(BaseBox):
-    def __init__(self, identifier, formal_parameters, block):
-        self.identifier = identifier
-        self.formal_parameters = formal_parameters
-        self.block = block
-
-    def print(self):
-        self.identifier.print()
-        self.formal_parameters.print()
-        self.block.print()
-
-
-class Function_decl(BaseBox):
-    def __init__(self, identifier, formal_parameters, dtype, block):
-        self.identifier = identifier
-        self.formal_parameters = formal_parameters
-        self.dtype = dtype
-        self.block = block
-
-    def print(self):
-        self.identifier.print()
-        self.formal_parameters.print()
-        self.dtype.print()
-        self.block.print()
-
-
-class Formal_parameters(BaseBox):
-    def __init__(self, param_section):
-        self.param_section = param_section
-
-    def print(self):
-        for param in self.param_section:
-            param.print()
-
-
-class Param_section(BaseBox):
-    def __init__(self, variable_decl, variable_decl_list):
-        self.variable_decl = variable_decl
-        self.variable_decl_list = variable_decl_list
-
-    def print(self):
-        self.variable_decl.print()
-        for var in self.variable_decl_list:
-            var.print()
-
-
-class Statement_part(BaseBox):
-    def __init__(self, compound_statement):
-        self.compound_statement = compound_statement
-
-    def print(self):
-        for statement in self.compound_statement:
-            statement.print()
-
-
-class Compound_statement(BaseBox):
-    def __init__(self, statement, statement_list):
-        self.statement = statement
-        self.statement_list = statement_list
-
-    def print(self):
-        for stat_list in self.statement_list:
-            stat_list.print()
-
-
-class Assignment_statement(BaseBox):
-    def __init__(self, variable, expression):
-        self.variable = variable
-        self.expression = expression
-
-    def print(self):
-        self.variable.print()
-        self.expression.print()
-
-
-class Function_procedure_statement(BaseBox):
-    def __init__(self, function_procedure_identifier, variable):
-        self.function_procedure_identifier = function_procedure_identifier
-        self.variable = variable
-
-    def print(self):
-        self.function_procedure_identifier.print()
-        self.variable.print()
-
-
-class Function_procedure_identifier(BaseBox):
-    def __init__(self, identifier):
-        self.identifier = identifier
-
-    def print(self):
-        self.identifier.print()
-
-
-class Read_statement(BaseBox):
-    def __init__(self, variable, variable_list):
-        self.variable = variable
-        self.variable_list = variable_list
-
-    def print(self):
-        self.identifier.print()
-        for var_list in variable_list:
-            var_list.print()
-
-
-class Write_statement(BaseBox):
-    def __init__(self, variable, variable_list):
-        self.variable = variable
-        self.variable_list = variable_list
-
-    def print(self):
-        self.variable.print()
-        for var_list in self.variable_list:
-            var_list.print()
-
-
-class If_statement(BaseBox):
-    def __init__(self, expression, statement):
-        self.expression = expression
-        self.statement = statement
-
-    def print(self):
-        self.expression.print()
-        self.statement.print()
-
-
-class If_else_statement(BaseBox):
-    def __init__(self, expression, statement, else_statement):
-        self.expression = expression
-        self.statement = statement
-        self.else_statement = else_statement
-
-    def print(self):
-        self.expression.print()
-        self.statement.print()
-        self.else_statement.print()
-
-
-class While_statement(BaseBox):
-    def __init__(self, expression, statement):
-        self.expression = expression
-        self.statement = statement
-
-    def print(self):
-        self.expression.print()
-        self.statement.print()
-
-
-class Simple_expression(BaseBox):
-    def __init__(self, sign, term, term_list):
-        self.sign = sign
-        self.term = term
-        self.term_list = term_list
-
-    def print(self):
-        self.sign.print()
-        self.term.print()
-        for term in self.term_list:
-            term.print()
-
-
-class Add(BinaryOp):
-    def eval(self):
-        return self.left.eval() + self.right.eval()
-
-
-class Sub(BinaryOp):
-    def eval(self):
-        return self.left.eval() - self.right.eval()
-
-
-class Mul(BinaryOp):
-    def eval(self):
-        return self.left.eval() * self.right.eval()
-
-
-class Div(BinaryOp):
-    def eval(self):
-        return self.left.eval() / self.right.eval()
-
-
-class Equals(BinaryOp):
-    def eval(self):
-        return self.left.eval() == self.right.eval()
-
-
-class Not_equal(BinaryOp):
-    def eval(self):
-        return self.left.eval() != self.right.eval()
-
-
-class Lesser_than(BinaryOp):
-    def eval(self):
-        return self.left.eval() < self.right.eval()
-
-
-class Lesser_or_equals(BinaryOp):
-    def eval(self):
-        return self.left.eval() <= self.right.eval()
-
-
-class Greater_than(BinaryOp):
-    def eval(self):
-        return self.left.eval() > self.right.eval()
-
-
-class Greater_or_equals(BinaryOp):
-    def eval(self):
-        return self.left.eval() >= self.right.eval()
-
-
-class Or(BinaryOp):
-    def eval(self):
-        return self.left.eval() or self.right.eval()
-
-
-class And(BinaryOp):
-    def eval(self):
-        return self.left.eval() and self.right.eval()
-
+from rply.token import BaseBox, TokenToken
+from objects import *
 
 pg = ParserGenerator(['PROGRAM',
                       'VAR',
@@ -362,10 +71,11 @@ def attrib(p):
 
 
 @pg.production('expression : NUMBER')
-def expression_number(p):
+def expression_integer(p):
     # p is a list of the pieces matched by the right hand side of the
     # rule
     return Number(int(p[0].getstr()))
+
 
 
 @pg.production('expression : OPEN_PARENS expression CLOSE_PARENS')
@@ -391,5 +101,38 @@ def expression_binop(p):
     else:
         raise AssertionError('Oops, this should not be possible!')
 
+
+@pg.production('expression : expression != expression')
+@pg.production('expression : expression == expression')
+@pg.production('expression : expression >= expression')
+@pg.production('expression : expression <= expression')
+@pg.production('expression : expression > expression')
+@pg.production('expression : expression < expression')
+@pg.production('expression : expression AND expression')
+@pg.production('expression : expression OR expression')
+
+def expression_logical_binop(p):
+    left = p[0]
+    right = p[2]
+    check = p[1]
+    
+    if check.gettokentype() == '==':
+        return Equals(left, right)
+    elif check.gettokentype() == '!=':
+        return Not_equal(left, right)
+    elif check.gettokentype() == '>=':
+        return Greater_or_equals(left, right)
+    elif check.gettokentype() == '<=':
+        return Less_or_equals(left, right)
+    elif check.gettokentype() == '>':
+        return Greater_than(left, right)
+    elif check.gettokentype() == '<':
+        return Less_than(left, right)
+    elif check.gettokentype() == 'AND':
+        return And(left, right)
+    elif check.gettokentype() == 'OR':
+        return Or(left, right)
+    else:
+        raise AssertionError("Oops, this shouldn't be possible")
 
 parser = pg.build()
